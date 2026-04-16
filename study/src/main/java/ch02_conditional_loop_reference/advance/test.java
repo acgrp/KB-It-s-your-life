0cd24e33f3b6995ccf0a66c5b6a1d7322eee4215
acgrp <acgrp103@naver.com>
2026-04-16 22:56:59 +0900

    학원 과제 동기화 및 패키지 구조 정리

---
+++ study/src/main/java/ch02_conditional_loop_reference/advance/test.java
@@ -0,0 +1,7 @@
+package main.java.ch02_conditional_loop_reference.advance;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+}
--- study/src/main/java/ch02_conditional_loop_reference/basic/ch06/sec01/ArrayRun.java
+++ study/src/main/java/ch02_conditional_loop_reference/basic/ch06/sec01/ArrayRun.java
@@ -1,8 +1,6 @@
 package main.java.ch02_conditional_loop_reference.basic.ch06.sec01;
-
-import main.java.ch02_conditional_loop_reference.basic.ch06.sec01.ArrayExample2;
-
 //class : 설계도
+
 public class ArrayRun { // 실행용 클래스
 
     public static void main(String[] args) {
