7d2082434cd2d12d104bf98725e8c241cbe3d8b6
acgrp <acgrp103@naver.com>
2026-04-21 21:53:22 +0900

    상속 복습 완료 (집)

---
+++ study/src/main/java/ch03_class/basic/ch07/sec08/exam01/Tire.java
@@ -0,0 +1,9 @@
+package main.java.ch03_class.basic.ch07.sec08.exam01;
+
+public class Tire{
+
+    public void roll() {
+        System.out.println("회전합니다.");
+    }
+
+}
---
+++ study/src/main/java/ch03_class/basic/ch07/sec08/exam02/Bus.java
@@ -0,0 +1,10 @@
+package main.java.ch03_class.basic.ch07.sec08.exam02;
+
+public class Bus extends Vehicle{
+
+    @Override
+    public void run() {
+        System.out.println("버스가 달립니다.");
+    }
+
+}
