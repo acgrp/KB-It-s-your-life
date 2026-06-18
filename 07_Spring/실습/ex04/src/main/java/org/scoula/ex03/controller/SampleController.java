da8247f1d64a6f21389177d6b157ca7f7a129e21
acgrp <acgrp103@naver.com>
2026-06-18 20:29:14 +0900

    수정

--- 07_Spring/실습/ex04/src/main/java/org/scoula/ex03/controller/SampleController.java
+++ 07_Spring/실습/ex04/src/main/java/org/scoula/ex03/controller/SampleController.java
@@ -2,9 +2,17 @@ package org.scoula.ex03.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.scoula.ex03.dto.SampleDTO;
+import org.scoula.ex03.dto.TodoDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 @Log4j2
@@ -20,12 +28,50 @@ public class SampleController {
 
     }
 
-    @GetMapping("/ex02List")
-    public String ex02List(SampleDTO sampleDTO) {
+    @GetMapping("/ex02")
+    public String ex02(String name, int age) {
 
-        log.info(id);
+        log.info(name);
+        log.info(age);
+
+        return "sample/ex02";
+
+    }
+
+    /* @GetMapping("/ex02List")
+     public String ex02List(String[] ids) {
+
+         log.info("String[] ids: " + Arrays.toString(ids));
+
+         return "sample/ex02List";
+
+     }   */
+    @GetMapping("/ex02List")
+    public String ex02List(@RequestParam("ids") ArrayList<String> ids) {// ArrayList 나 List는 RequestParam를 붙여야함
+        log.info("ArrayList<String> ids : " + ids);
 
         return "sample/ex02List";
+
+    }
+
+    @GetMapping("/ex03")
+    public String ex03(TodoDTO todoDTO) {
+
+        log.info(todoDTO);
+
+        return "sample/ex03List";
+
+    }
+
+    @GetMapping("/ex07")
+    @ResponseBody
+    public SampleDTO ex07() {
+
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.setName("AAA");
+        sampleDTO.setAge(14);
+
+        return sampleDTO;
 
     }
 }
