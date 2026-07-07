a53da216e3ad15cb4fe62b62abf883d7f93f7a93
jinseo <acgrp103@gmail.com>
2026-07-07 17:48:00 +0900

    0707 board 완료

---
+++ 07_Spring/ex/untitled/src/main/java/org/scoula/security/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package org.scoula.security.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+    // 문자셋필터
+    public CharacterEncodingFilter encodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
+
+        http.authorizeRequests()
+                .antMatchers("/security/all").permitAll()
+                .antMatchers("/security/admin").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/security/member").access("hasAnyRole('ROLE_MEMBER', 'ROLE_ADMIN')");
+
+        http.formLogin() // 로그인 폼설정시작
+                .loginPage("/security/login")
+                .loginProcessingUrl("/security/login")
+                .defaultSuccessUrl("/");
+// 로그아웃설정시작
+        http.logout().logoutUrl("/security/logout")// POST: 로그아웃 호출 url
+                .invalidateHttpSession(true) // 세션 invalidate
+                .deleteCookies("remember-me", "JSESSION-ID") // 삭제할 쿠키 목록
+                .logoutSuccessUrl("/security/logout"); // GET: 로그아웃 이후이동할페이지
+
+
+
+    }
+
+
+}
---
+++ 08_Vue_Spring/0707/backend/src/main/webapp/resources/assets/TravelListPage-TPbFEx-g.js
@@ -0,0 +1,1 @@
+import{_ as e,b as r,o as t}from"./index-DBwYsUe7.js";const a={};function c(n,o){return t(),r("h1",null,"TravelListPage")}const _=e(a,[["render",c]]);export{_ as default};
